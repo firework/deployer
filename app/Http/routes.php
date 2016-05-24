d0837b3aa91804638f2769bd50ab633345e04d1a
Vinicius Domingues <vinicius.ld@outlook.com>
2016-05-24 18:12:05 -0300

    Auth created

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -1,18 +1,10 @@
 <?php
+Route::auth();
 
-/*
-|--------------------------------------------------------------------------
-| Application Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register all of the routes for an application.
-| It's a breeze. Simply tell Laravel the URIs it should respond to
-| and give it the controller to call when that URI is requested.
-|
-*/
-
-Route::get('/', 'DeployController@index');
-Route::post('/deploy', 'DeployController@deployIt');
-Route::get('/command', 'DeployController@deployCommand');
-Route::post('/save_command', 'DeployController@saveCommand');
-Route::get('/status', 'DeployController@status');
+Route::group(['middleware'=> 'auth'], function(){
+    Route::get('/', 'DeployController@index');
+    Route::post('/deploy', 'DeployController@deployIt');
+    Route::get('/command', 'DeployController@deployCommand');
+    Route::post('/save_command', 'DeployController@saveCommand');
+    Route::get('/status', 'DeployController@status');
+});
