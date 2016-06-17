575025116a270e670e723b3d5935bf06356a2449
Vinicius Domingues <vinicius.ld@outlook.com>
2016-06-17 17:34:24 -0300

    fixed bad identation

--- app/Jobs/DeploymentQueueJob.php
+++ app/Jobs/DeploymentQueueJob.php
@@ -19,20 +19,20 @@ class DeploymentQueueJob extends Job implements ShouldQueue
     protected $deploy;
 
     /**
-    * Create a new job instance.
-    *
-    * @return void
-    */
+     * Create a new job instance.
+     *
+     * @return void
+     */
     public function __construct($deploy)
     {
         $this->deploy = $deploy;
     }
 
     /**
-    * Execute the job.
-    *
-    * @return void
-    */
+     * Execute the job.
+     *
+     * @return void
+     */
     public function handle()
     {
         Log::info("Handling queue...");
