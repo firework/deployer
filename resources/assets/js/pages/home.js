f1ea3c73150c07e592bc1386b79e84cfe8864898
Gabriel Oliveira <gab.oliveira@outlook.com>
2017-02-03 17:07:18 -0200

    changes requested

--- resources/assets/js/pages/home.js
+++ resources/assets/js/pages/home.js
@@ -2,73 +2,70 @@
     var showDialogButton = document.querySelector('#fire'),
         formDeploy = document.querySelector('form'),
         serverSelect = document.querySelector('#select-server'),
-        branchSelect = document.querySelector('#select-branch'),
         taskSelect = document.querySelector('#select-task'),
+        branchSelect = document.querySelector('#select-branch'),
         progressBar = document.querySelector('#progress-bar');
 
     if (showDialogButton) {
         var dialog = new DeployerConfirmDialog();
 
         showDialogButton.addEventListener('click', function(event) {
+
             event.preventDefault();
 
-            if(serverSelect.value === "-1") {
-                serverSelect.parentNode.classList.add('is-invalid');
-                return;
-            } else {
-                serverSelect.parentNode.classList.remove('is-invalid');
+            var error = false;
+            var selects = [ serverSelect, taskSelect, branchSelect ];
+
+            selects.forEach(function(select) {
+                if(select.value === "-1") {
+                    select.parentNode.classList.add('is-invalid');
+                    error = true;
+                } else {
+                    select.parentNode.classList.remove('is-invalid');
+                }
+            }, this);
+
+            if(!error) {
+                dialog.showModal(function() {
+                    formDeploy.submit();
+                });
             }
 
-            if(taskSelect.value === "-1") {
-                taskSelect.parentNode.classList.add('is-invalid');
-                return;
-            } else {
-                taskSelect.parentNode.classList.remove('is-invalid');
-            }
-
-            if(branchSelect.value === "-1") {
-                branchSelect.parentNode.classList.add('is-invalid');
-                return;
-            } else {
-                branchSelect.parentNode.classList.remove('is-invalid');
-            }
-
-            dialog.showModal(function() {
-                formDeploy.submit();
-            }.bind(this));
-        }.bind(this));
+        });
     }
 
     if(serverSelect) {
         serverSelect.addEventListener("change", function() {
             fillBranches(serverSelect.value);
-        }.bind(this));
+        });
     }
 
     function fillBranches(serverId) {
-        progressBar.className = 'is-loading';
+        progressBar.classList.add('is-loading');
         branchSelect.disabled = true;
+
         axios.get('/server/' + serverId + '/branches')
             .then(function(response) {
-                progressBar.className = '';
+
+                progressBar.classList.remove('is-loading');
+
                 if(response && response.data) {
+                    var defaultOpt = branchSelect.querySelector("[disabled]");
+
                     branchSelect.options.length = 0;
 
-                    var msg = document.createElement('option');
-                    msg.innerHTML = 'Select a Branch';
-                    msg.disabled = true;
-                    msg.selected = true;
-                    msg.value = -1;
-                    branchSelect.appendChild(msg);
+                    branchSelect.appendChild(defaultOpt);
 
                     response.data.forEach(function(branch) {
                         var opt = document.createElement('option');
                         opt.innerHTML = branch;
                         opt.value = branch;
                         branchSelect.appendChild(opt);
-                    }, this);
+                    });
+
                     branchSelect.disabled = false;
+
                 }
-            }.bind(this));
+            });
     }
 })();
